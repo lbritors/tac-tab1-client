3eaf2ffa5a802ffb2808bfb7ba2e8b1b0727f014
lbritors <britoleticiars@gmail.com>
2025-05-19 08:59:07 -0300

    feat: add post_message client function

--- auth_client.py
+++ auth_client.py
@@ -1,34 +1,69 @@
 import getpass
+import os
+
 import aiohttp
 import asyncio
+from dotenv import load_dotenv
 
-async def authenticate():
+load_dotenv()
 
-    auth_url = ""
+API_URL = os.getenv("API_URL")
+AUTH_URL = os.getenv("AUTH_URL")
+
+async def authenticate(session):
+
     username = input("Digite seu login: ")
     password = getpass.getpass("Digite sua senha: ")
     scenario = input("Escolha o a forma de autenticação [hmac/rsa]: ").lower()
-
+    if scenario not in ["rsa", "hmac"]:
+        print("Cenário inválido. User 'hmac' ou 'rsa'.")
+        return None,None
     data = {
         "username": username,
-        "password": password
+        "password": password,
+        "scenario": scenario
     }
 
     try:
-        async with aiohttp.ClientSession() as session:
-            async with session.post(auth_url, json=data) as response:
-                if response.status == 200:
-                    res = await response.json()
-                    token = res.get("token")
-                    if token:
-                        print("Autenticação deu certo!")
-                        print("Token: ", token)
-                    else:
-                        print("Autenticação não deu certo!")
+        async with session.post(AUTH_URL, json=data, ssl=False) as response:
+            if response.status == 200:
+                res = await response.json()
+                token = res.get("token")
+                scenario = res.get("scenario")
+                if token:
+                    print("Autenticação deu certo!")
+                    return token,scenario
                 else:
-                    print(f"Erro na autenticação: {response.status} - {await response.text()} ")
+                    print("Autenticação não deu certo!Token não encontrado na resposta")
+            else:
+                text = await response.text()
+                print(f"Erro na autenticação: {response.status} - {text} ")
     except aiohttp.ClientError as error:
         print(f"Erro ao conectar ao servidor: {error}")
+    return None, None
+
+
+async def post_message(session, token, scenario, api_url = API_URL):
+    content = input("Digite a mensagem confidencial: ")
+    data = {"content": content}
+    headers = {
+        "Authorization": f"Bearer {token}",
+        "X-Auth-Scenario": scenario
+    }
+
+    try:
+        async with session.post(api_url, json=data, headers=headers, ssl=False) as response:
+            if response.status == 201:
+                res = await response.json()
+                print("Mensagem criada com sucesso!\n")
+                print(res)
+            else:
+                text = await response.text()
+                print(f"Erro ao criar mensagem: {response.status} - {text}")
+    except aiohttp.ClientError as error:
+        print(f"Erro ao conectar à API: {error}")
+
+
 
 async def main():
     await authenticate()
